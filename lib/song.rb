42fa237f0851e18a3202ee51ed1e963a0e9d2411
Robinson Regalado <rregalado17@gmail.com>
2019-08-15 15:48:54 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -30,7 +30,13 @@ class Song
   
   def self.genre_count
     genre_count = {}
-    @@genres.each {|genre| genre_count[genre]}
+    @@genres.each do |genre|
+      if genre_count[genre]
+        genre_count[genre] += 1 
+      else
+        genre_count[genre] = 1
+      end
+    end
     genre_count
   end
   
